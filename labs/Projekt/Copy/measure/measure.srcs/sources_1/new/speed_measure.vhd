9d2ec31628869d4424503149fbfa57c4e2d5b987
student <student@PC-628.urel.feec.vutbr.cz>
2022-04-12 14:44:06 +0200

    Projekt

---
+++ labs/Projekt/Copy/measure/measure.srcs/sources_1/new/speed_measure.vhd
@@ -0,0 +1,145 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use ieee.numeric_std.all;
+
+
+entity speed_measure is
+    port(
+        clk         : in  std_logic;
+        senzor_1    : in  std_logic;
+        senzor_2    : in  std_logic;
+        senzor_3    : in  std_logic;
+        senzor_4    : in  std_logic
+    
+    );
+end speed_measure;
+
+architecture Behavioral of speed_measure is
+    -- Cas straveny v useku
+    signal s_cnt_1      : natural;
+    signal s_cnt_2      : natural;
+    signal s_cnt_3      : natural;
+    
+    -- Zapocate mereni
+    signal s_merime     : std_logic;
+    
+    -- Zapocete mereni v danem useku
+    signal s_usek1      : std_logic;
+    signal s_usek2      : std_logic;
+    signal s_usek3      : std_logic;
+    
+    -- Rychlost
+    signal v1           : unsigned(15 downto 0);
+    signal v1_dp        : unsigned(3 downto 0);
+    signal v2           : unsigned(15 downto 0);
+    signal v2_dp        : unsigned(3 downto 0);
+    signal v3           : unsigned(15 downto 0);
+    signal v3_dp        : unsigned(3 downto 0);
+    signal v_avg        : unsigned(15 downto 0);
+    signal v_avg_dp     : unsigned(3 downto 0);
+   
+   
+    signal reset        : std_logic;
+
+    -- Delka useku v cm
+    constant c_DELKA_USEK_1 : unsigned(7 downto 0) := b"0000_0010";
+
+
+begin
+
+p_reset : process(reset)
+begin
+    if (reset = '1') then
+        s_merime <= '0';
+   
+        s_cnt_1 <= 0;
+        s_cnt_2 <= 0;
+        s_cnt_3 <= 0;
+        s_cnt_2 <= 0;
+           
+        s_usek1 <= '0';
+        s_usek2 <= '0';
+        s_usek3 <= '0';
+       
+            
+        reset <= '0';
+        
+            
+
+    end if;
+end process p_reset;
+
+p_measure : process(clk)
+begin
+    if rising_edge(clk) then
+        if (s_merime = '1') then
+            if (s_usek1 = '1') then
+                s_cnt_1 <= s_cnt_1 + 1;
+                
+            elsif (s_usek2 = '1') then
+                s_cnt_2 <= s_cnt_2 + 1;
+                
+            elsif (s_usek3 = '1') then
+                s_cnt_3 <= s_cnt_3 + 1;
+                
+            end if;
+        end if;
+    end if;
+end process p_measure;
+
+p_measure_1_enable : process(senzor_1)
+begin
+    if (senzor_1 = '1') then
+        -- reset
+        if (s_merime = '1') then
+            reset <= '1';
+        else
+            s_merime <= '1';
+            s_usek1 <= '1';
+        end if;
+    end if;          
+end process p_measure_1_enable;
+
+p_measure_2_enable : process(senzor_2)
+begin
+    if (senzor_2 = '1') then
+        if (s_merime = '1') then
+            -- Vyhodnoceni prvniho useku
+            if (s_usek1 = '1') then
+                --v1 <= c_DELKA_USEK_1/(s_cnt_1*100000000);
+                s_usek1 <= '0';
+            end if;
+            s_usek2 <= '1';
+        end if;
+    end if;          
+end process p_measure_2_enable;
+
+p_measure_3_enable : process(senzor_3)
+begin
+    if (senzor_3 = '1') then
+        if (s_merime = '1') then
+            -- Vyhodnoceni druheho useku
+            if (s_usek2 = '1') then
+                --v2 <= c_DELKA_USEK_1/(s_cnt_2*100000000);
+                s_usek2 <= '0';
+            end if;
+            s_usek3 <= '1';
+        end if;
+    end if;          
+end process p_measure_3_enable;
+
+p_measure_vyhodnoceni : process(senzor_4)
+begin
+    if (senzor_4 = '1') then
+        if (s_merime = '1') then
+            -- Vyhodnoceni tretiho useku
+            if (s_usek3 = '1') then
+                --v3 <= c_DELKA_USEK_1/(s_cnt_3*100000000);
+                s_usek3 <= '0';
+                --v_avg <= (v1+v2+v3)/3;
+            end if;
+        end if;
+    end if;          
+end process p_measure_vyhodnoceni;
+    
+end architecture Behavioral;
---
+++ labs/Projekt/measure/measure.srcs/sim_1/new/tb_speed_measure.vhd
@@ -0,0 +1,110 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 04/12/2022 02:12:51 PM
+-- Design Name: 
+-- Module Name: tb_speed_measure - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use ieee.numeric_std.all;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity tb_speed_measure is
+--  Port ( );
+end tb_speed_measure;
+
+architecture Behavioral of tb_speed_measure is
+     -- Local constants
+    constant c_CLK_100MHZ_PERIOD : time := 10 ns;
+
+    --Local signals
+    signal s_clk_100MHz : std_logic;
+    signal s_senzor_1   : std_logic;
+    signal s_senzor_2   : std_logic;
+    signal s_senzor_3   : std_logic;
+    signal s_senzor_4   : std_logic;
+    signal s_reset      : std_logic;
+ 
+begin
+    uut_speed_measure : entity work.speed_measure
+        port map(
+            clk     => s_clk_100MHz,
+    
+            senzor_1 => s_senzor_1,
+            senzor_2 => s_senzor_2,
+            senzor_3 => s_senzor_3,
+            senzor_4 => s_senzor_4,
+            reset => s_reset
+        );
+        
+    p_clk_gen : process
+    begin
+        while now < 10000 ns loop -- 10 usec of simulation
+            s_clk_100MHz <= '0';
+            wait for c_CLK_100MHZ_PERIOD / 2;
+            s_clk_100MHz <= '1';
+            wait for c_CLK_100MHZ_PERIOD / 2;
+        end loop;
+        wait;
+    end process p_clk_gen;
+    
+    p_stimulus : process
+    begin
+        s_senzor_1 <= '0';
+        s_senzor_2 <= '0';
+        s_senzor_3 <= '0';
+        s_senzor_4 <= '0';
+        
+        wait for 10 ns;
+        s_reset <= '1';
+        wait for 10 ns;
+        s_reset <= '0';
+    
+        wait for 50 ns;
+        s_senzor_1 <= '1';
+        wait for 50 ns;
+        s_senzor_1 <= '0';
+        
+        
+        wait for 200 ns;
+        s_senzor_2 <= '1';
+        wait for 50 ns;
+        s_senzor_2 <= '0';
+        
+        wait for 120 ns;
+        s_senzor_3 <= '1';
+        wait for 30 ns;
+        s_senzor_3 <= '0';
+        
+        wait for 250 ns;
+        s_senzor_4 <= '1';
+        wait;
+    end process p_stimulus;
+
+end architecture Behavioral;
+
+
+    
